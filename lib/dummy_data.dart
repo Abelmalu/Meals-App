94679eda6fc36e74e685dc79497df50077eecab9
abel malu <abellmalu@gmail.com>
2024-04-24 15:56:19 +0300

    categories model and dummy data created

---
+++ lib/dummy_data.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'models/category_screen.dart';
+
+List DUMMY_CATEGORIES = [
+  CategoryRecipe(
+    id: 'c1',
+    title: 'Ethiopian',
+    color: Colors.purple,
+  ),
+  CategoryRecipe(
+    id: 'c2',
+    title: 'Quic and Easy',
+    color: Colors.green,
+  ),
+  CategoryRecipe(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.blue,
+  ),
+];
